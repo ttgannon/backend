dc326d17617b69dcf11d3ecd36180fcc4c08a405
ttgannon <ga1179@nyu.edu>
2023-07-28 14:22:54 -0400

    sql queries intro

--- sql-querying/queries_playstore.sql
+++ sql-querying/queries_playstore.sql
@@ -1,1 +1,50 @@
+
 -- Comments in SQL Start with dash-dash --
+--1
+SELECT * FROM analytics WHERE id = 1880;
+
+--2
+SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+
+--3
+SELECT category, COUNT(*) AS count FROM analytics GROUP BY category;
+
+--4
+SELECT * FROM analytics ORDER BY reviews DESC LIMIT 5;
+
+--5
+SELECT * FROM analytics WHERE rating >= 4.8 ORDER BY reviews DESC LIMIT 1;
+
+--6
+SELECT category,AVG(rating) FROM analytics GROUP BY category ORDER BY AVG(rating) DESC;
+
+--7
+SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price DESC LIMIT 1;
+
+--8
+SELECT * FROM analytics WHERE min_installs <= 50 AND rating IS NOT NULL ORDER BY rating DESC;
+
+--9
+SELECT app_name FROM analytics WHERE rating < 3 AND reviews >= 10000;
+
+--10
+SELECT * FROM analytics WHERE price BETWEEN .10 AND 1.00 ORDER BY reviews DESC LIMIT 10;
+
+--11
+SELECT * FROM analytics ORDER BY last_updated ASC LIMIT 1;
+
+--12
+SELECT * FROM analytics ORDER BY price DESC LIMIT 1;
+
+--13
+SELECT SUM(reviews) FROM analytics;
+
+--14
+SELECT category FROM analytics GROUP BY category HAVING COUNT(*) > 300;
+
+--15
+SELECT app_name, reviews, min_installs, min_installs/reviews AS proportion 
+FROM analytics 
+WHERE min_installs >= 100000 
+ORDER BY proportion DESC
+LIMIT 1;
--- sql-querying/queries_products.sql
+++ sql-querying/queries_products.sql
@@ -1,49 +1,46 @@
--- Comments in SQL Start with dash-dash --
 --1
-SELECT * FROM analytics WHERE id = 1880;
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('chair', '44.00', 'false');
+
 
 --2
-SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('stool', '25.99', 'true');
 
 --3
-SELECT category, COUNT(*) AS count FROM analytics GROUP BY category;
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('table', '124.00', 'false');
 
 --4
-SELECT * FROM analytics ORDER BY reviews DESC LIMIT 5;
+SELECT * FROM products;
 
 --5
-SELECT * FROM analytics WHERE rating >= 4.8 ORDER BY reviews DESC LIMIT 1;
+SELECT name FROM products;
 
 --6
-SELECT category,AVG(rating) FROM analytics GROUP BY category ORDER BY AVG(rating) DESC;
+SELECT name,price FROM products;
 
 --7
-SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price DESC LIMIT 1;
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('couch', '499.00', 'false');
 
 --8
-SELECT * FROM analytics WHERE min_installs <= 50 AND rating IS NOT NULL ORDER BY rating DESC;
+SELECT * FROM products WHERE can_be_returned = true;
 
 --9
-SELECT app_name FROM analytics WHERE rating < 3 AND reviews >= 10000;
+SELECT * FROM products WHERE price < 44;
 
 --10
-SELECT * FROM analytics WHERE price BETWEEN .10 AND 1.00 ORDER BY reviews DESC LIMIT 10;
+SELECT * FROM products WHERE price > 22.50 AND price < 99.99;
 
 --11
-SELECT * FROM analytics ORDER BY last_updated ASC LIMIT 1;
+UPDATE products SET price = price - 20;
 
 --12
-SELECT * FROM analytics ORDER BY price DESC LIMIT 1;
+DELETE FROM products WHERE price < 25;
 
 --13
-SELECT SUM(reviews) FROM analytics;
+UPDATE products SET price = price + 20;
 
 --14
-SELECT category FROM analytics GROUP BY category HAVING COUNT(*) > 300;
-
---15
-SELECT app_name, reviews, min_installs, min_installs/reviews AS proportion 
-FROM analytics 
-WHERE min_installs >= 100000 
-ORDER BY proportion DESC
-LIMIT 1;
+UPDATE products SET can_be_returned = 'true';
